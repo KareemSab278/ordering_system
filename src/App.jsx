a7cbe12909c75687b16b71c4f3e17d9426549ad5
Kareem Elsabrouty <kareem@coinadrink.co.uk>
2026-03-04 10:07:50 +0000

    reduce fullscreen delay

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       getCurrentWindow().setFullscreen(true);
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
