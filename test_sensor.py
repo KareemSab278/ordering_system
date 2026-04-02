9519cf8f88d4dc5432be61fe5d1379148c906c4b
KareemSab278 <kareemsab278@gmail.com>
2026-04-02 13:17:55 +0100

    added motion sensor test

---
+++ test_sensor.py
@@ -0,0 +1,27 @@
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    raise SystemExit("RPi.GPIO module not found. Install with: sudo apt install python3-rpi.gpio")
+
+GPIO.setmode(GPIO.BOARD)
+PIR_PIN = 7 
+
+GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print(f"PIR motion sensor test started on pin {PIR_PIN} (mode={GPIO.getmode()})")
+print("Press Ctrl+C to exit")
+
+try:
+    while True:
+        motion = GPIO.input(PIR_PIN)
+        if motion:
+            print("Motion detected!")
+        else:
+            print("No motion")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("\nExiting test")
+finally:
+    GPIO.cleanup()
